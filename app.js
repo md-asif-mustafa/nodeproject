407a2b8905ce9c457cfdd68c97b9191f70d6cd84
Asif Mustafa <mdasif.april@gmail.com>
2024-07-16 20:32:23 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 // app.js
 const http = require('http');
 
-const hostname = '172.31.38.17';
+const hostname = '54.196.16.121';
 const port = 3000;
 
 const server = http.createServer((req, res) => {
